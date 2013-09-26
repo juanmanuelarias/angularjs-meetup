0d2c169cda4df2aa1b6f8ee8d9957c413c11917e
Juan Manuel Arias <juanmanuelarias@gmail.com>
2013-09-25 23:44:08 -0300

    Service example with code

--- meetup-01/presentation/app/js/services/TaskData.js
+++ meetup-01/presentation/app/js/services/TaskData.js
@@ -5,6 +5,7 @@
             { title: 'Fix bug #3446', status: 'done', date: new Date() },
             { title: 'Add new Feature', status: 'pending', date: new Date() }],
         add: function (task) {
+            task.date = new Date();
             this.tasks.push(task);
         }
     }
