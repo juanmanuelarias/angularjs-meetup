5bbad05992d3d33103a545a243d912d9694849c8
Juan Manuel Arias <juanmanuelarias@gmail.com>
2013-11-25 17:38:30 -0300

    Add presentation files

---
+++ meetup-02 - frameworks & tools/demo/app/scripts/directives/myDirective.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('meetupApp')
+  .directive('myDirective', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the myDirective directive');
+      }
+    };
+  });
